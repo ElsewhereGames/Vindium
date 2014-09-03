69dce2f67750d8d6838ab7befc5efcbad7893a6a
Hans Pragt <hpragt@gmail.com>
2014-09-02 22:42:09 -0700

    Added a new project to add a GUI to the vindinium models.

---
+++ Vindinium GUI/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+#region Using
+
+using System.Windows;
+
+using Elsewhere.Vindinium.GUI.View;
+
+#endregion Using
+
+namespace Elsewhere.Vindinium.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        #region Life-Cycle
+
+        /// <summary>
+        /// When the application is started, data and view models are created
+        /// and assigned to views.
+        /// </summary>
+        /// <param name="arguments">None used.</param>
+        protected override void OnStartup(StartupEventArgs arguments)
+        {
+            VindiniumView window = new VindiniumView();
+            window.Show();
+        }
+
+        #endregion Life-Cycle
+
+    }
+}
---
+++ Vindinium GUI/View/VindiniumView.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+#region Using
+
+using System.Windows;
+
+#endregion Using
+
+namespace Elsewhere.Vindinium.GUI.View
+{
+
+    /// <summary>
+    /// Interaction logic for VindiniumView.xaml
+    /// </summary>
+    public partial class VindiniumView : Window
+    {
+
+        #region Life-Cycle
+
+        /// <summary>
+        /// Creates a default instance of the Vindinium view.
+        /// </summary>
+        public VindiniumView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Life-Cycle
+
+    }
+}
